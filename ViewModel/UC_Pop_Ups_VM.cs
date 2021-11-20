f3afea54fb7ce4bd02fa478a0e2d2e636ae43085
志伟 何 <1098303467@qq.com>
2021-11-21 01:14:48 +0800

    1.优化逻辑

--- ViewModel/UC_Pop_Ups_VM.cs
+++ ViewModel/UC_Pop_Ups_VM.cs
@@ -1,4 +1,5 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using HanGao.View.User_Control.Pop_Ups;
 using PropertyChanged;
 using System;
@@ -6,7 +7,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace HanGao.ViewModel
 {
@@ -26,7 +29,65 @@ namespace HanGao.ViewModel
         public int Page { set; get; } = 1;
 
         //public UserControl Pop_UserControl { set; get; } = new UC_Sink_Size() { };
-        public UserControl Pop_UserControl { set; get; } = new UC_Sink_Craft_List() { };
+        public UserControl Pop_UserControl { set; get; } = new Sink_Type() { };
 
+
+
+
+
+
+
+        /// <summary>
+        /// 标题枚举属性
+        /// </summary>
+        public RadioButton_Name RB_Name { set; get; } = RadioButton_Name.水槽类型选择;
+
+
+
+
+        /// <summary>
+        /// 标题枚举
+        /// </summary>
+        public enum RadioButton_Name
+        {
+            水槽类型选择,
+            水槽尺寸调节,
+            工艺参数调节
+        }
+
+
+
+        /// <summary>
+        /// 侧边栏打开关闭事件命令
+        /// </summary>
+        public ICommand RB_Check_Comm
+        {
+            get => new RelayCommand<RoutedEventArgs>((Sm) =>
+            {
+
+                //把参数类型转换控件
+                RadioButton e = Sm.Source as RadioButton;
+
+                switch (int.Parse((string)e.Tag))
+                {
+                    case (int)RadioButton_Name.水槽类型选择:
+                        Pop_UserControl= new Sink_Type() { };
+                        break;
+                    case (int)RadioButton_Name.水槽尺寸调节:
+                        Pop_UserControl = new UC_Sink_Size() { };
+                        break;
+                    case (int)RadioButton_Name.工艺参数调节:
+                        Pop_UserControl = new UC_Sink_Craft_List() { };
+
+                        break;
+                    default:
+                        break;
+                }
+
+
+
+
+            });
+        }
     }
 }
