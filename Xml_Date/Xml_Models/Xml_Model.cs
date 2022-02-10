31113cf9d03e09ac871a3ae36edd5766e89d12da
志伟 何 <1098303467@qq.com>
2022-02-10 17:36:24 +0800

    添加XMl文件

---
+++ Xml_Date/Xml_Models/Xml_Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanGao.Xml_Date.Xml_Models
+{
+    public  class Xml_Model
+    {
+
+
+
+
+
+
+
+
+    }
+}
