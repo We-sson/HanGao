6284d9768845ef17952db67a4f573d4de2f0413c
志伟 何 <1098303467@qq.com>
2022-09-12 17:23:53 +0800

    1.更改项目框架错误问题

--- ViewModel/UC_Vision_CameraSet_ViewModel.cs
+++ ViewModel/UC_Vision_CameraSet_ViewModel.cs
@@ -46,68 +46,68 @@ namespace HanGao.ViewModel
         /// <summary>
         /// 查找网络内相机
         /// </summary>
-        public ICommand Initialize_GIGE_Camera_Comm
+        public ICommand Read_Image_Comm
         {
             get => new RelayCommand<RoutedEventArgs>((Sm) =>
             {
                 //把参数类型转换控件
 
-                // ch:创建设备列表 | en:Create Device List
-                System.GC.Collect();
+                //// ch:创建设备列表 | en:Create Device List
+                //System.GC.Collect();
 
 
-                MV_CC_DEVICE_INFO_LIST DeviceList =new MV_CC_DEVICE_INFO_LIST ();
-                DeviceList.nDeviceNum = 0;
+                //MV_CC_DEVICE_INFO_LIST DeviceList =new MV_CC_DEVICE_INFO_LIST ();
+                //DeviceList.nDeviceNum = 0;
 
 
-                //获得设备枚举
-                int nRet=  CSystem.EnumDevices(CSystem.MV_GIGE_DEVICE, ref Camera_List);
+                ////获得设备枚举
+                //int nRet=  CSystem.EnumDevices(CSystem.MV_GIGE_DEVICE, ref Camera_List);
 
                 
 
-                int nRet = CCamera.CreateHandle(CCamera.MV_GIGE_DEVICE | CCamera.MV_USB_DEVICE, ref m_stDeviceList);
-                if (0 != nRet)
-                {
-                    ShowErrorMsg("Enumerate devices fail!", 0);
-                    return;
-                }
+                //int nRet = CCamera.CreateHandle(CCamera.MV_GIGE_DEVICE | CCamera.MV_USB_DEVICE, ref m_stDeviceList);
+                //if (0 != nRet)
+                //{
+                //    ShowErrorMsg("Enumerate devices fail!", 0);
+                //    return;
+                //}
 
-                // ch:在窗体列表中显示设备名 | en:Display device name in the form list
-                for (int i = 0; i < m_stDeviceList.nDeviceNum; i++)
-                {
-                    MyCamera.MV_CC_DEVICE_INFO device = (MyCamera.MV_CC_DEVICE_INFO)Marshal.PtrToStructure(m_stDeviceList.pDeviceInfo[i], typeof(MyCamera.MV_CC_DEVICE_INFO));
-                    if (device.nTLayerType == MyCamera.MV_GIGE_DEVICE)
-                    {
-                        MyCamera.MV_GIGE_DEVICE_INFO gigeInfo = (MyCamera.MV_GIGE_DEVICE_INFO)MyCamera.ByteToStruct(device.SpecialInfo.stGigEInfo, typeof(MyCamera.MV_GIGE_DEVICE_INFO));
+                //// ch:在窗体列表中显示设备名 | en:Display device name in the form list
+                //for (int i = 0; i < m_stDeviceList.nDeviceNum; i++)
+                //{
+                //    MyCamera.MV_CC_DEVICE_INFO device = (MyCamera.MV_CC_DEVICE_INFO)Marshal.PtrToStructure(m_stDeviceList.pDeviceInfo[i], typeof(MyCamera.MV_CC_DEVICE_INFO));
+                //    if (device.nTLayerType == MyCamera.MV_GIGE_DEVICE)
+                //    {
+                //        MyCamera.MV_GIGE_DEVICE_INFO gigeInfo = (MyCamera.MV_GIGE_DEVICE_INFO)MyCamera.ByteToStruct(device.SpecialInfo.stGigEInfo, typeof(MyCamera.MV_GIGE_DEVICE_INFO));
 
-                        if (gigeInfo.chUserDefinedName != "")
-                        {
-                            cbDeviceList.Items.Add("GEV: " + gigeInfo.chUserDefinedName + " (" + gigeInfo.chSerialNumber + ")");
-                        }
-                        else
-                        {
-                            cbDeviceList.Items.Add("GEV: " + gigeInfo.chManufacturerName + " " + gigeInfo.chModelName + " (" + gigeInfo.chSerialNumber + ")");
-                        }
-                    }
-                    else if (device.nTLayerType == MyCamera.MV_USB_DEVICE)
-                    {
-                        MyCamera.MV_USB3_DEVICE_INFO usbInfo = (MyCamera.MV_USB3_DEVICE_INFO)MyCamera.ByteToStruct(device.SpecialInfo.stUsb3VInfo, typeof(MyCamera.MV_USB3_DEVICE_INFO));
-                        if (usbInfo.chUserDefinedName != "")
-                        {
-                            cbDeviceList.Items.Add("U3V: " + usbInfo.chUserDefinedName + " (" + usbInfo.chSerialNumber + ")");
-                        }
-                        else
-                        {
-                            cbDeviceList.Items.Add("U3V: " + usbInfo.chManufacturerName + " " + usbInfo.chModelName + " (" + usbInfo.chSerialNumber + ")");
-                        }
-                    }
-                }
+                //        if (gigeInfo.chUserDefinedName != "")
+                //        {
+                //            cbDeviceList.Items.Add("GEV: " + gigeInfo.chUserDefinedName + " (" + gigeInfo.chSerialNumber + ")");
+                //        }
+                //        else
+                //        {
+                //            cbDeviceList.Items.Add("GEV: " + gigeInfo.chManufacturerName + " " + gigeInfo.chModelName + " (" + gigeInfo.chSerialNumber + ")");
+                //        }
+                //    }
+                //    else if (device.nTLayerType == MyCamera.MV_USB_DEVICE)
+                //    {
+                //        MyCamera.MV_USB3_DEVICE_INFO usbInfo = (MyCamera.MV_USB3_DEVICE_INFO)MyCamera.ByteToStruct(device.SpecialInfo.stUsb3VInfo, typeof(MyCamera.MV_USB3_DEVICE_INFO));
+                //        if (usbInfo.chUserDefinedName != "")
+                //        {
+                //            cbDeviceList.Items.Add("U3V: " + usbInfo.chUserDefinedName + " (" + usbInfo.chSerialNumber + ")");
+                //        }
+                //        else
+                //        {
+                //            cbDeviceList.Items.Add("U3V: " + usbInfo.chManufacturerName + " " + usbInfo.chModelName + " (" + usbInfo.chSerialNumber + ")");
+                //        }
+                //    }
+                //}
 
-                // ch:选择第一项 | en:Select the first item
-                if (m_stDeviceList.nDeviceNum != 0)
-                {
-                    cbDeviceList.SelectedIndex = 0;
-                }
+                //// ch:选择第一项 | en:Select the first item
+                //if (m_stDeviceList.nDeviceNum != 0)
+                //{
+                //    cbDeviceList.SelectedIndex = 0;
+                //}
 
 
 
